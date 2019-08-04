b9f3eb998bef91131d170530d26de518ae4e5260
marszm <mariusz.szmer@gmail.com>
2019-08-04 10:49:45 +0200

    added Create, Retreive operation

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,19 @@ public class Main {
 
     public static void main(final String[] args) {
 
+        //Create Users
         UserDAO userDAO = new UserDAO();
-        userDAO.addUser(1,"a","bb");
-//        System.out.println();
+        userDAO.addUser(1,"Mariusz","Szmer");
+        userDAO.addUser(2,"Ewa","Szmer");
+        userDAO.addUser(3,"Leszek","Szmer");
+
+        //Retreive Users
+        userDAO.retriveAllUsers();
+
+        //Update User
+
+
+
+        //Delete User
     }
 }
--- src/UserDAO.java
+++ src/UserDAO.java
@@ -4,6 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import javax.persistence.Entity;
+import java.util.List;
 
 @Entity
 public class UserDAO {
@@ -11,7 +12,6 @@ public class UserDAO {
     private static SessionFactory sessionFactory;
 
     public void addUser(int id, String fname, String lname) {
-//        User user = null;
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = null;
 
@@ -27,6 +27,24 @@ public class UserDAO {
             if(transaction != null)
                 transaction.rollback();
                 e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void retriveAllUsers() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            List users = session.createQuery("from User").list();
+            users.stream().forEach(System.out::println);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if(transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
         } finally {
             session.close();
         }
